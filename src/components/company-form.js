93ca33a5e39b73055ef0740c9a265108bcab800e
Sherif Sarhan <sherif.a.sarhan@gmail.com>
2018-03-24 11:56:37 -0400

    add form and server calls

--- src/components/company-form.js
+++ src/components/company-form.js
@@ -1,22 +1,49 @@
 import React from 'react'
 import { Form, Button, Grid } from 'semantic-ui-react'
 
-const CompanyForm = () => (
-    <Grid columns={2}>
-        <Grid.Column>
-            <Grid.Row>
-                <Form>
-                    <Form.Group>
-                    <Form.Input label='First name' placeholder='First Name' width={6} />
-                    <Form.Input label='Middle Name' placeholder='Middle Name' width={4} />
-                    <Form.Input label='Last Name' placeholder='Last Name' width={6} />
-                    </Form.Group>
-                </Form>
-                <Button>Submit</Button>
-            </Grid.Row>
-        </Grid.Column>
-    </Grid>
-)
+class CompanyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {firstName: '', middleName: '', lastName: ''};
+  
+      this.handleChangeFirst = this.handleChangeFirst.bind(this);
+      this.handleChangeMiddle = this.handleChangeMiddle.bind(this);
+      this.handleChangeLast = this.handleChangeLast.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChangeFirst(event, data) {
+      this.setState({firstName: data.value});
+    }
+
+    handleChangeMiddle(event, data) {
+        this.setState({middleName: data.value});
+    }
+
+    handleChangeLast(event, data) {
+        this.setState({lastName: data.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.firstName + this.state.middleName + this.state.lastName);
+      fetch('http://localhost:3001/saveName', {method: 'POST', body: JSON.stringify(this.state)})
+        .then((response) => console.log(response));
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+            <Form.Input label='First name' placeholder='First Name' value={this.state.firstName} onChange={this.handleChangeFirst} width={6} />
+            <Form.Input label='Middle Name' placeholder='Middle Name' value={this.state.middleName} onChange={this.handleChangeMiddle} width={4} />
+            <Form.Input label='Last Name' placeholder='Last Name' value={this.state.lastName} onChange={this.handleChangeLast} width={6} />
+        </Form.Group>
+          <Button type="submit" value="Submit">Submit</Button>
+        </Form>
+      );
+    }
+  }
 
 export default CompanyForm
     
