5ee0416f46c21e32facabcf0e8c151d765dc3c85
Sherif Sarhan <sherif.a.sarhan@gmail.com>
2018-03-27 18:26:05 -0400

    submit contact, add gender dropdown, age picker, success toast

--- src/components/company-form.jsx
+++ src/components/company-form.jsx
@@ -1,56 +1,64 @@
 import React, { Component } from 'react';
-import { Form, Input, Button } from 'semantic-ui-react';
+import { Form, Input, Button, Message, Transition, Dropdown } from 'semantic-ui-react';
 
 class CompanyForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { firstName: '', middleName: '', lastName: '' };
+        this.state = { name: '', age: '', gender: '', visible: false };
 
-        this.handleChangeFirst = this.handleChangeFirst.bind(this);
-        this.handleChangeMiddle = this.handleChangeMiddle.bind(this);
-        this.handleChangeLast = this.handleChangeLast.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeAge = this.handleChangeAge.bind(this);
+        this.handleChangeGender = this.handleChangeGender.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChangeFirst(event, data) {
-        this.setState({ firstName: data.value });
+    handleChangeName(event, data) {
+        this.setState({ name: data.value });
     }
 
-    handleChangeMiddle(event, data) {
-        this.setState({ middleName: data.value });
+    handleChangeAge(event, data) {
+        this.setState({ age: data.value });
     }
 
-    handleChangeLast(event, data) {
-        this.setState({ lastName: data.value });
+    handleChangeGender(event, data) {
+        this.setState({ gender: data.value });
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.firstName + this.state.middleName + this.state.lastName);
-        fetch('http://localhost:3001/saveName', { method: 'POST', body: JSON.stringify(this.state) })
+        // alert('A name was submitted: ' + this.state.name + this.state.age + this.state.gender);
+        fetch('http://localhost:3001/addContact', { method: 'POST', body: JSON.stringify(this.state) })
             .then((response) => console.log(response));
         event.preventDefault();
-        console.log("dsa");
+        this.setState({ visible: true });
     }
 
     render() {
+        const genderOptions = [{ key: 'Male', value: 'Male', text: 'Male' }, { key: 'Female', value: 'Female', text: 'Female' }];
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group widths='equal'>
-                    <Form.Field>
-                        <label>First name</label>
-                        <Input fluid placeholder='First Name' value={this.state.firstName} onChange={this.handleChangeFirst} />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Middle name</label>
-                        <Input fluid placeholder='Middle Name' value={this.state.middleName} onChange={this.handleChangeMiddle} />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Last name</label>
-                        <Input fluid placeholder='Last name' value={this.state.lastName} onChange={this.handleChangeLast} />
-                    </Form.Field>
-                </Form.Group>
-                <Button type="submit" value="Submit">Submit</Button>
-            </Form>
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group widths='equal'>
+                        <Form.Field>
+                            <label>Name</label>
+                            <Input fluid placeholder='Name' value={this.state.name} onChange={this.handleChangeName} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Age</label>
+                            <Input fluid type="number" min="0" max="200" placeholder='Age' value={this.state.age} onChange={this.handleChangeAge} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Gender</label>
+                            <Dropdown fluid placeholder='Gender' value={this.state.gender} selection options={genderOptions} onChange={this.handleChangeGender} />
+                        </Form.Field>
+                    </Form.Group>
+                    <Button type="submit" value="Submit">Submit</Button>
+                </Form>
+                <Transition visible={this.state.visible} animation='scale' duration={500}>
+                    <Message positive>
+						Contact Submitted Successfully!
+                    </Message>
+                </Transition>
+            </div >
         );
     }
 }
